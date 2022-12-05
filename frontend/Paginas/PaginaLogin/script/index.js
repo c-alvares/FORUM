955512cbab6c48d1770700a246119fdf5f3039cc
BeaGabriela <biiagabrielaa@gmail.com>
2022-12-05 09:55:20 -0300

    frontend: continuacao

--- frontend/Paginas/PaginaLogin/script/index.js
+++ frontend/Paginas/PaginaLogin/script/index.js
@@ -5,29 +5,40 @@ var btnLogar = document.querySelector('#logar')
 var InputsVazioEmail = document.querySelector('#vazioEmail')
 var InputsVaziosSenha = document.querySelector('#vazioSenha')
 var loginErrado = document.querySelector('#userErrado')
+var abrirModal = document.querySelector('.abrir')
+var certo = false;
 
+fetch('http://localhost:3000/forum/listar')
+.then(res => {return res.json()})
+.then(re => {
+    re.forEach( login => {
+        btnLogar.addEventListener('click', () => {
+            if (InputEmail.value.lenght < 0) {
+                InputsVazioEmail.innerHTML = "Por favor, preencha o campo acima!"
+            }
+            else if (InputSenha.value.lenght < 0) {
+                InputsVaziosSenha.innerHTML = "Por favor, preencha o campo acima!"
+            } else {
+                if (InputEmail.value == login.user_name || InputEmail.value == login.email && InputSenha.value == login.senha) {
+                    certo = true;
+                    localStorage.setItem('username', login.user_name)
+                    window.location.href = "../paginaHome/home.html"
+                   
+                } else {
+                    certo = false
+                    nav.classList.add('a-jump-bounce')
+                    loginErrado.innerHTML = 'Login ou senha incorretos!'
+                }
+            }
+        
+        })
+    })
+})
 
-var teste = {
-    email: "Roberto@gmail.com",
-    username: 'Robert12',
-    senha: '1234teste'
+function AbrirModalRedefinirSenha(){
+     abrirModal.classList.remove('modal')
+     logar.style =  ''
 }
 
-btnLogar.addEventListener('click', () => {
-    if (InputEmail.value.lenght < 0) {
-        InputsVazioEmail.innerHTML = "Por favor, preencha o campo acima!"
-    }
-    else if (InputSenha.value.lenght < 0) {
-        InputsVaziosSenha.innerHTML = "Por favor, preencha o campo acima!"
-    } else {
-        if (InputEmail.value == teste.email && InputSenha.value == teste.senha) {
-            localStorage.setItem('username', teste.username)
-            window.location.href = '../../PaginaHome/index.html'
-        } else {
-            nav.classList.add('a-jump-bounce')
-            loginErrado.innerHTML = 'Login ou senha incorretos!'
-        }
-    }
 
-})
 
