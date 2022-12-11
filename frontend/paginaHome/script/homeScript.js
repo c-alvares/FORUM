9b00e89de8b04ae333d83a2be3ae98c627b513c3
BeaGabriela <biiagabrielaa@gmail.com>
2022-12-11 15:46:31 -0300

    Mais uma tentaiva de fazer as respostas, consegui, mas com varios erros...

--- frontend/paginaHome/script/homeScript.js
+++ frontend/paginaHome/script/homeScript.js
@@ -13,74 +13,66 @@ let tabelaPesquisa = document.querySelector('.tabelaPesquisa');
 //   let iniciarBusca = {
 //     "nome": barra.value
 //   }
-  const options = {
-    method: 'GET'
-    // 'headers': {'Content-Type': 'application/json'},
-    // 'body': JSON.stringify(iniciarBusca)
-  };
+const options = {
+  method: 'GET'
+  // 'headers': {'Content-Type': 'application/json'},
+  // 'body': JSON.stringify(iniciarBusca)
+};
 
-  fetch('http://localhost:3000/listarTemas', options)
+fetch('http://localhost:3000/listarTemas', options)
 
-    .then(response => response.json())
-    .then(response => {
-      response.forEach(temaPesquisado => {
-        let abrirDiv = document.querySelector('.linhaPesquisa').cloneNode(true)
-        abrirDiv.classList.remove('modal2')
-        abrirDiv.querySelector('#celulaPesquisa').innerHTML = temaPesquisado.nome
+  .then(response => response.json())
+  .then(response => {
+    response.forEach(temaPesquisado => {
+      let abrirDiv = document.querySelector('.linhaPesquisa').cloneNode(true)
+      abrirDiv.classList.remove('modal2')
+      abrirDiv.querySelector('#celulaPesquisa').innerHTML = temaPesquisado.nome
 
-        tabelaPesquisa.appendChild(abrirDiv)
-      })
+      tabelaPesquisa.appendChild(abrirDiv)
     })
-    .catch(err => console.error(err));
-    
-  
+  })
+  .catch(err => console.error(err));
 
 
 
-  
-  function publicarRespResposta(){
-  var responderResp = document.querySelector('#inppublicar')
- 
+
+
+
+function publicarResposta() {
   fetch('http://localhost:3000/listarResposta')
     .then(response => response.json())
     .then(respons => {
       respons.forEach(respostas => {
+                var responderRe = document.querySelector('#inppublicar')
 
-        fetch('http://localhost:3000/listarRespostaRes')
-        .then(response => response.json())
-        .then(response => {
-          response.forEach(respresp => {
-          if(respostas.id_resposta == respresp.id_resposta){
-            console.log(responderResp.value)
-  var responder = {
-    "id_resposta": respostas.id_resposta,
-    "resposta_res": responderResp.value
-  }
-  console.log(responder)
-          }
+                 var responder = {
+                  "id_publi": respostas.id_publi,
+                  "resposta": responderRe.value
+                 }
+                 
 
-  // fetch('http://localhost:3000/criarTemas', {
-  //   'method': 'POST',
-  //   'headers': {
-  //     'Content-Type': 'application/json'
-  //   },
-  //   'body': JSON.stringify(criarTemas)
-  // })
+                 fetch('http://localhost:3000/criarResposta', {
+                'method': 'POST',
+                'headers': {
+                  'Content-Type': 'application/json'
+                },
+                'body': JSON.stringify(responder)
+              })
 
-  //   .then(response => response.status)
-  //   .then(response => {
-  //     if (response == 201) {
-  //       alert("Tema cadastrado com sucesso")
-  //       window.location.reload()
-  //     } else {
-  //       alert('Falha ao cadastrar novo tema')
-  //     }
-  //   })
-  //   .catch(err => console.error(err));
-})
+                .then(response => response.status)
+                .then(response => {
+                  if (response == 201) {
+                    alert("Resposta publicada com sucesso")
+      
+                  } else {
+                    alert('Falha ao publicar resposta')
+                  }
+                })
+                .catch(err => console.error(err));
+               
+          
       })
     })
-})
 }
 
 
@@ -140,13 +132,13 @@ function carregarCards() {
         .then(response => response.json())
         .then(response => {
           response.forEach(p => {
-            let publicacoes = document.querySelector('.publicacoes').cloneNode(true);
-            publicacoes.classList.remove('modal');
             fetch('http://localhost:3000/forum/listar')
               .then(response => response.json())
               .then(response => {
                 response.forEach(user => {
                   if (p.id_user == user.id_user) {
+                    let publicacoes = document.querySelector('.publicacoes').cloneNode(true);
+                    publicacoes.classList.remove('modal');
                     publicacoes.querySelector('#publicacao').innerHTML = p.publicacoes;
                     publicacoes.querySelector("#avatarPubli").src = '../../../assets/' + user.img
                     publicacoes.querySelector("#userPubli").innerHTML = user.user_name
@@ -161,17 +153,17 @@ function carregarCards() {
 }
 
 function VisualizarRespostas() {
-  var RewsRespos = document.querySelector('#visualizarRespostasRespostas')
-  
   fetch('http://localhost:3000/listarPublicacoes')
     .then(response => response.json())
     .then(response => {
       response.forEach(p => {
+         var per = document.querySelector('#resPergunta')
+              per.innerHTML = p.publicacoes
         fetch('http://localhost:3000/listarResposta')
           .then(response => response.json())
           .then(response => {
             response.forEach(resp => {
-              fetch('http://localhost:3000/forum/listar')
+                           fetch('http://localhost:3000/forum/listar')
                 .then(response => response.json())
                 .then(response => {
                   response.forEach(us => {
@@ -184,10 +176,9 @@ function VisualizarRespostas() {
                       resposta.querySelector('#avatarPubli').src = '../../../assets/' + us.img
                       resposta.querySelector('#usuarioPergunta').innerHTML = us.user_name
                       resposta.querySelector('#resp').innerHTML = resp.resposta
-                      resposta.querySelector('#resPergunta').innerHTML = p.publicacoes
 
-                      document.querySelector('.caminhoResp').appendChild(resposta)
-                      }
+                      document.querySelector('#arrumandoQuadrado').appendChild(resposta)
+                    }
                   })
                 })
             })
@@ -196,37 +187,48 @@ function VisualizarRespostas() {
     })
 }
 
-function visualizarRespResp(){
+function visualizarRespResp() {
+  var RespostasRed = document.querySelector('.respostasRespostass').cloneNode(true)
+  RespostasRed.classList.remove('modal')
+
   fetch('http://localhost:3000/forum/listar')
+    .then(response => response.json())
+    .then(respons => {
+      respons.forEach(us => {
+        fetch('http://localhost:3000/listarPublicacoes')
+          .then(response => response.json())
+          .then(response => {
+            response.forEach(p => {
+              if(us.id_user == p.id_user){
+                console.log(us)
+              
+              fetch('http://localhost:3000/listarRespostaRes')
                 .then(response => response.json())
-                .then(response => {
-                  response.forEach(us => {
-                    fetch('http://localhost:3000/listarRespostaRes')
-                          .then(response => response.json())
-                          .then(response => {
-                            response.forEach(rr => {
-                              fetch('http://localhost:3000/listarResposta')
-                              .then(response => response.json())
-                              .then(response => {
-                                response.forEach(resp => {
-                                  if(rr.id_resposta == resp.id_resposta){
-                                    console.log(resp.id_resposta)
-                              var RespostasRed = document.querySelector('#respostasRespostass')
-                              RespostasRed.cloneNode(true)
-                              RespostasRed.classList.remove('modal')
+                .then(respon => {
+                  respon.forEach(rr => {
+                    console.log(rr)
+                    fetch('http://localhost:3000/listarResposta')
+                      .then(response => response.json())
+                      .then(response => {
+                        response.forEach(resp => {
+                          console.log(rr)
+                          if (rr.id_resposta == resp.id_resposta) {
+                            
+                            RespostasRed.querySelector('#avatarRes').src = '../../../assets/' + us.img
+                            RespostasRed.querySelector('#userRes').innerHTML = us.user_name
+                            RespostasRed.querySelector('#ResRes').innerHTML = rr.resposta_res
 
-                              RespostasRed.querySelector('#avatarRes').src = '../../../assets/' + us.img
-                              RespostasRed.querySelector('#userRes').innerHTML = us.user_name
-                              RespostasRed.querySelector('#ResRes').innerHTML = rr.resposta_res
-
-                              document.querySelector('.caminhoResp').appendChild(RespostasRe)
-                                  }
+                            document.querySelector('.caminhoResp').appendChild(RespostasRed)
+                          } 
+                        })
+                      })
                   })
                 })
-                })
-              })
-              })
-                  })
+              }
+            })
+          })
+      })
+    })
 }
 
 
@@ -241,7 +243,12 @@ const fecharModalTema = () => {
 }
 
 
-function fecharResp(){
+function fecharResp() {
   let c = document.querySelector('.caminhoResp')
- c.classList.add('modal')
+  c.classList.add('modal')
+}
+
+function fecharresposta(){
+  let fr= document.querySelector('.respostasRespostass')
+  fr.classList.add('modal')
 }
