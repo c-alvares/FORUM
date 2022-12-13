34c65dc98e5f4adce0d8d6ecd7e913860dbb4aea
c-alvares <cyrobrandaoalvares@gmail.com>
2022-12-13 08:13:52 -0300

    Criada estruta de telas mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+import Login from './src/screens/loginScreen/login';
+
+
+const Stack = createNativeStackNavigator();
+
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Login" component={Login} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+
+  return (
+    <NavigationContainer>
+      {/* <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />     
+      </Stack.Navigator> */}
+      <MyTabs />
+    </NavigationContainer>
+  )
+}
---
+++ mobile/src/screens/loginScreen/login.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { View, TextInput, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import styles from './styles/style';
+
+export default function Login() {
+
+
+    return (
+        <View style={styles.container}>
+            <Text>Teste</Text>
+        </View>
+    )
+}
