cf5cd5376da3992767d49b06a26eaffb4c7b35cc
c-alvares <cyrobrandaoalvares@gmail.com>
2022-12-15 11:35:20 -0300

    flork

--- mobile/App.js
+++ mobile/App.js
@@ -24,7 +24,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Home" component={Home} />     
         <Stack.Screen name="Login" component={Login} />     
       </Stack.Navigator>
